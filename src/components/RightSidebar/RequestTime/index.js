da589c7949112d337d7790d9e4edaba958676d98
Justin Gauthier <itsme@justingauthier.com>
2018-05-20 01:33:53 -0400

    Before refactor of request time component

--- src/components/RightSidebar/RequestTime/index.js
+++ src/components/RightSidebar/RequestTime/index.js
@@ -13,11 +13,19 @@ class RequestTime extends Component {
     }
 
     this.openStartDate = this.openStartDate.bind(this);
+    this.openEndDate = this.openEndDate.bind(this);
   }
 
   openStartDate() {
     this.setState(prevState => ({
       startDateOpen: !prevState.startDateOpen
+    }));
+    console.log("state", this.state);
+  }
+
+  openEndDate() {
+    this.setState(prevState => ({
+      endDateOpen: !prevState.endDateOpen
     }));
     console.log("state", this.state);
   }
@@ -29,13 +37,18 @@ class RequestTime extends Component {
       startClass.push('show-accordion');
     }
 
+    const endClass = ['accordion-content'];
+    if(this.state.endDateOpen) {
+      endClass.push('show-accordion');
+    }
+
     return (
       <div className="request-time">
         <form>
           <div className="padding-container text-right">
             <label>
               All Day
-              <input type="checkbox" name="allDay" />
+              <input type="checkbox" name="allDay" checked />
             </label>
           </div>
 
@@ -50,6 +63,21 @@ class RequestTime extends Component {
                 <p>thiasdfa sdf as dfasdf as d fasdfasdfa sdfasdf as dfasdf as df asdf</p>
               </div>
             </div>
+            <div className="accordion-item">
+              <div className="accordion-title" onClick={this.openEndDate}>
+                {moment().format('dddd, MMMM D YYYY')}
+                <img src={rightArrow} alt="right arrow" className={this.state.endDateOpen ? 'accordion-active': ''} />
+              </div>
+              
+              <div className={endClass.join(' ')}>
+                <p>thiasdfa sdf as dfasdf as d fasdfasdfa sdfasdf as dfasdf as df asdf</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="padding-container">
+            <textarea name="message" placeholder="Your Message" />
+            <button>Request Time</button>
           </div>
         </form>
       </div>
